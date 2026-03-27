7c470bd8572697ee1f7d25f8d5ae3d8479bd7ae1
gsjsjjssshshsj-stack <gsjsjjssshshsj@gmail.com>
2026-03-27 09:59:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,119 +1,27 @@
-let workers = JSON.parse(localStorage.getItem("workers")) || []
+let balance = 2000;
 
-function showPage(page){
+function addMoney(){
 
-document.querySelectorAll(".page").forEach(p=>{
-p.style.display="none"
-})
+let amount = document.getElementById("amount").value;
 
-document.getElementById(page).style.display="block"
+balance = balance + Number(amount);
 
-}
-
-showPage("home")
-
-function addWorker(){
-
-let name = document.getElementById("name").value
-let skill = document.getElementById("skill").value
-let phone = document.getElementById("phone").value
-let photoInput = document.getElementById("photo")
-
-let photoURL = ""
-
-if(photoInput.files.length > 0){
-photoURL = URL.createObjectURL(photoInput.files[0])
-}
-
-let worker = {name,skill,phone,photo:photoURL}
-
-workers.push(worker)
-
-localStorage.setItem("workers",JSON.stringify(workers))
-
-displayWorkers()
-
-alert("Worker Registered Successfully")
+document.getElementById("balance").innerText = "₹" + balance;
 
 }
 
-function displayWorkers(){
+function withdrawMoney(){
 
-let list = document.getElementById("workerList")
+let amount = document.getElementById("amount").value;
 
-list.innerHTML=""
+balance = balance - Number(amount);
 
-workers.forEach(w=>{
-
-let rating = Math.floor(Math.random()*5)+1
-
-list.innerHTML += `
-<div class="worker">
-
-<img src="${w.photo}" width="80">
-
-<h3>${w.name}</h3>
-
-<p>Skill: ${w.skill}</p>
-
-<p>Phone: ${w.phone}</p>
-
-<p>Rating: ${rating} ⭐</p>
-
-<a href="tel:${w.phone}">Call Worker</a>
-
-<br><br>
-
-<a href="https://www.google.com/maps/search/${w.skill} near me" target="_blank">
-View Location
-</a>
-
-</div>
-`
-
-})
+document.getElementById("balance").innerText = "₹" + balance;
 
 }
 
-function searchWorker(){
+function applyJob(){
 
-let search = document.getElementById("search").value.toLowerCase()
-
-let filtered = workers.filter(w =>
-w.skill.toLowerCase().includes(search)
-)
-
-let list = document.getElementById("workerList")
-
-list.innerHTML=""
-
-filtered.forEach(w=>{
-
-list.innerHTML += `
-<div class="worker">
-
-<h3>${w.name}</h3>
-
-<p>Skill: ${w.skill}</p>
-
-<p>Phone: ${w.phone}</p>
-
-</div>
-`
-
-})
+alert("Job Application Sent!");
 
 }
-
-function postJob(){
-
-let customer = document.getElementById("customer").value
-let job = document.getElementById("jobdesc").value
-
-let jobList = document.getElementById("jobList")
-
-jobList.innerHTML += `<p><b>${customer}</b> needs: ${job}</p>`
-
-}
-
-displayWorkers()
